6a01d40f86f63aecfc654bf20082cf97bd1f427f
Georgios Patios <giorgospatios@gmail.com>
2023-10-26 15:06:09 +0300

    weather trends problem and added comments to heavy rainfall

--- heavy_rainfall.py
+++ heavy_rainfall.py
@@ -3,7 +3,9 @@ heights = [int(num) for num in heights.split()]
 
 n = len(heights)
 left_max = [0 for i in range(n)]
+# in the i-th place I store the biggest height that is left of the i-th height (or its self)
 right_max = [0 for j in range(n)]
+# same but for the right
 
 left_max[0] = heights[0]
 right_max[n-1] = heights[n-1]
@@ -11,6 +13,8 @@ right_max[n-1] = heights[n-1]
 for i in range(1, n):
     if heights[i] == 0:
         left_max[i] = 0
+        # this way in the next positions the height its self will store in the i-th position so as to not add water as
+        # long as the heights go only up after a zero - same goes with right matrix
     else:
         left_max[i] = max(left_max[i-1], heights[i])
 
@@ -23,5 +27,6 @@ for i in range(n-2, -1, -1):
 total = 0
 for i in range(1, n-1):
     total += min(left_max[i], right_max[i]) - heights[i]
-    print(total, end=' ')
+    # the water added is at most the difference with the lowest max height from left or right
 
+print("Total water:", total)
---
+++ weather_trends.py
@@ -0,0 +1,40 @@
+temperatures = input("Give temperatures (e.g. 2 4 5 6 0 4) : ")
+span = int(input("Give maximum span of days: "))
+temperatures = [float(num) for num in temperatures.split()]
+
+left=0
+right=1
+max_pos_diff = -1  # if this number never exceeds 0 this means there was no temperature increase
+temp_len = len(temperatures)
+
+if span <= 1:
+    print("Span must be at least 2")
+elif span == 2:  # if span is 2 then the days are always one after the other, so it is implemented differently
+    for i in range(1, temp_len):
+        diff = temperatures[i] - temperatures[i-1]
+        if diff > max_pos_diff:
+            max_pos_diff = diff
+else:
+    while left != right:
+        diff = temperatures[right] - temperatures[left]
+        if diff > max_pos_diff:
+            max_pos_diff = diff
+        if right == temp_len - 1:  # through this the while loop will ultimately end
+            left += 1  # right has reached the end and left is catching up until left = right
+        elif left == right - 1:  # the pointers are side by side so the right one must go up
+            right += 1
+        elif left == right - span + 1:  # right - left has reached the maximum day - distance so left must go up
+            # e.g. span = 3 so the two days must be at most 2 days apart
+            left += 1
+        elif temperatures[right] - temperatures[left + 1] >= temperatures[right + 1] - temperatures[left]:
+            # this is where all the juice is at
+            # the pointer that goes up is the one that makes the biggest increase in temperature in the next
+            # comparison
+            left += 1
+        else:
+            right += 1
+
+if max_pos_diff <= 0:
+    print("There was no increase in temperature")
+else:
+    print("Max temperature increase :", max_pos_diff, "degrees")
